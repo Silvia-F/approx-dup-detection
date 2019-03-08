ac61eea13a40c0d8ceb762bbbdbe650c63a188fe
Silvia-F <sicaf96@gmail.com>
2019-03-08 11:23:28 +0000

    Add SmithWaterman skeleton. Refer #11

---
+++ src/main/java/org/pentaho/dataintegration/SmithWaterman.java
@@ -0,0 +1,12 @@
+package org.pentaho.dataintegration;
+
+public class SmithWaterman {
+	
+	private final String matrix = "placeholder";
+	private final int startGap = 5;
+	private final int continueGap = 1;
+	
+	public double calcSimilarity(String s1, String s2) {
+		return 0.1;
+	}
+}
